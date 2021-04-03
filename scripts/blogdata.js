ddf5930dde3441f3408445230fb6feb432da1dfc
ryanbakerucalgary <ryan.baker1@ucalgary.ca>
2021-04-02 21:33:19 -0600

    General grammar and beautification

--- scripts/blogdata.js
+++ scripts/blogdata.js
@@ -9,8 +9,8 @@ const blogdata = [
     },
     {
         id:1, //store HTML file as '[id#].html' in 'posts' folder
-        title:'How I Saved Thanksgiving', 
-        type:'Reflection', //'Project' or 'Reflection'
+        title:'Saving Thanksgiving', 
+        type:'Project', //'Project' or 'Reflection'
         date:'October 8, 2018',
         desc:'Two Ovens Failed at the Same Time!', // keep it less than 10 words
         img:'img/thanksgiving_card.JPG', //should be 4:3 ratio
