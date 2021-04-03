86b449b34d9318ddc3237802e3348aa32dc8240e
ryanbakerucalgary <ryan.baker1@ucalgary.ca>
2021-04-02 21:08:28 -0600

    ENCM 369 Post

--- scripts/blogdata.js
+++ scripts/blogdata.js
@@ -1,5 +1,13 @@
 const blogdata = [
     {
+        id:2, //store HTML file as '[id#].html' in 'posts' folder
+        title:'Looking Back on ENCM 369', 
+        type:'Reflection', //'Project' or 'Reflection'
+        date:'April 2, 2021',
+        desc:'This was my favourite course of 2021!', // keep it less than 10 words
+        img:'img/mips_card.jpg', //should be 4:3 ratio
+    },
+    {
         id:1, //store HTML file as '[id#].html' in 'posts' folder
         title:'How I Saved Thanksgiving', 
         type:'Reflection', //'Project' or 'Reflection'
