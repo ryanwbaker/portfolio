9a87fd02a34d775b360bb3446d29e19dbfc53d57
ryanbakerucalgary <ryan.baker1@ucalgary.ca>
2021-04-18 23:54:21 -0600

    Post 5

--- scripts/blogdata.js
+++ scripts/blogdata.js
@@ -1,34 +1,41 @@
-const blogdata = [
-    {
-        id:4, //store HTML file as '[id#].html' in 'posts' folder
-        title:'Looking Back on ENEL 361', 
-        type:'Reflection', //'Project' or 'Reflection'
-        date:'April 2, 2021',
-        desc:'So this is why we studied chemistry...', // keep it less than 10 words
-        img:'img/361_card.jpg', //should be 4:3 ratio
+const blogdata = [{
+        id: 5, //store HTML file as '[id#].html' in 'posts' folder
+        title: 'Agile Project Management',
+        type: 'Reflection', //'Project' or 'Reflection'
+        date: 'April 18, 2021',
+        desc: 'A Practical System With a Fitting Name', // keep it less than 10 words
+        img: 'img/agile-pm-card.png', //should be 4:3 ratio
     },
     {
-        id:3, //store HTML file as '[id#].html' in 'posts' folder
-        title:'Looking Back on ENEL 327', 
-        type:'Reflection', //'Project' or 'Reflection'
-        date:'April 2, 2021',
-        desc:'Signals and Transforms: Challenging but Rewarding', // keep it less than 10 words
-        img:'img/327_card.jpg', //should be 4:3 ratio
+        id: 4, //store HTML file as '[id#].html' in 'posts' folder
+        title: 'Looking Back on ENEL 361',
+        type: 'Reflection', //'Project' or 'Reflection'
+        date: 'April 2, 2021',
+        desc: 'So this is why we studied chemistry...', // keep it less than 10 words
+        img: 'img/361_card.jpg', //should be 4:3 ratio
     },
     {
-        id:2, //store HTML file as '[id#].html' in 'posts' folder
-        title:'Looking Back on ENCM 369', 
-        type:'Reflection', //'Project' or 'Reflection'
-        date:'April 2, 2021',
-        desc:'This was my favourite ILS course!', // keep it less than 10 words
-        img:'img/mips_card.jpg', //should be 4:3 ratio
+        id: 3, //store HTML file as '[id#].html' in 'posts' folder
+        title: 'Looking Back on ENEL 327',
+        type: 'Reflection', //'Project' or 'Reflection'
+        date: 'April 2, 2021',
+        desc: 'Signals and Transforms: Challenging but Rewarding', // keep it less than 10 words
+        img: 'img/327_card.jpg', //should be 4:3 ratio
     },
     {
-        id:1, //store HTML file as '[id#].html' in 'posts' folder
-        title:'Saving Thanksgiving', 
-        type:'Project', //'Project' or 'Reflection'
-        date:'October 8, 2018',
-        desc:'Two Ovens Failed at the Same Time!', // keep it less than 10 words
-        img:'img/thanksgiving_card.JPG', //should be 4:3 ratio
+        id: 2, //store HTML file as '[id#].html' in 'posts' folder
+        title: 'Looking Back on ENCM 369',
+        type: 'Reflection', //'Project' or 'Reflection'
+        date: 'April 2, 2021',
+        desc: 'This was my favourite ILS course!', // keep it less than 10 words
+        img: 'img/mips_card.jpg', //should be 4:3 ratio
+    },
+    {
+        id: 1, //store HTML file as '[id#].html' in 'posts' folder
+        title: 'Saving Thanksgiving',
+        type: 'Project', //'Project' or 'Reflection'
+        date: 'October 8, 2018',
+        desc: 'Two Ovens Failed at the Same Time on THANKSGIVING DAY!', // keep it less than 10 words
+        img: 'img/thanksgiving_card.JPG', //should be 4:3 ratio
     }
 ];
